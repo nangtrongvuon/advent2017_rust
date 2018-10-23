7b7a61538e72f022b8ce07727c588801a71fef84
Le Viet Hoang Dung <dungleviethoang@gmail.com>
2018-10-23 22:31:57 +0700

    Add WIP for floor 3

--- advent_floor3/src/main.rs
+++ advent_floor3/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     manhanttanDistance(361527);
+    fillSquares(2);
 }
 
 fn manhanttanDistance(input: usize) {
@@ -23,4 +24,73 @@ fn manhanttanDistance(input: usize) {
     };
 
     println!("{:?}", distance_from_corner_to_center + distance_from_center_to_input);
+}
+
+fn fillSquares(input: usize) {
+    let mut step = 1;
+    let mut current_number: i32 = 1;
+
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+
+    let mut current_values = vec![[x, y, current_number]];
+
+    while current_number < input as i32 {
+        // Move right by step
+        for i in 0..step {
+            current_number = sumNeighbours(&current_values.last().unwrap(), &current_values);
+            x += 1;
+            current_values.push([x, y, current_number]);
+        }
+
+        // Move up by step
+        for i in 0..step {
+            current_number = sumNeighbours(&current_values.last().unwrap(), &current_values);
+            y += 1;
+
+            current_values.push([x, y, current_number]);
+        }           
+
+        // Move left by step + 1
+        for i in 0..(step + 1) {
+            current_number = sumNeighbours(&current_values.last().unwrap(), &current_values);
+            x -= 1;
+
+            current_values.push([x, y, current_number]);
+        }
+
+        // Move down by step + 1 
+        for i in 0..(step + 1) {
+            current_number = sumNeighbours(&current_values.last().unwrap(), &current_values);
+            y -= 1;
+
+            current_values.push([x, y, current_number]);
+        }
+    }
+
+    println!("{:?}", current_values);
+}
+
+fn sumNeighbours(input: &[i32; 3], current_values: &Vec<[i32; 3]>) -> i32 {
+
+    let input_x = input[0];
+    let input_y = input[1];
+
+    let mut result = 1;
+
+    for value in current_values {
+        let x = value[0];
+        let y = value[1];
+        let num = value[2];
+
+        println!("input: {:?}", input);
+        println!("{:?} {:?}", x - input_x, y - input_y);
+
+        if (x - input_x).abs() == 1 || (y - input_y).abs() == 1 {
+            println!("num to add: {:?}", num);
+            result += num;
+        }
+    }
+    println!("====");
+    return result;
 }
